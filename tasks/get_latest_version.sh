562fa4f658f6861d82ad83aa2d50b3a34f67cbbf
kota65535 <kota65535@gmail.com>
2016-12-20 18:11:54 +0900

    support ansible 2.2.0

--- tasks/get_latest_version.sh
+++ tasks/get_latest_version.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
-# Ensure this script runs in the become_user's home directory
-cd ~
-# Source rbenv initialization script
-source $1
-version_regex_str=$(echo "$2" | sed -e 's/x/[[:digit:]]/g' -e 's/\./\\./g')
+tmpdir=$(mktemp -d)
+git clone -q $1 ${tmpdir}
+git clone -q $2 ${tmpdir}/plugins/ruby-build
+export RBENV_ROOT=${tmpdir}
+export PATH="$RBENV_ROOT/bin:$PATH"
+eval "$(${RBENV_ROOT}/bin/rbenv init -)"
+
+version_regex_str=$(echo "$3" | sed -e 's/x/[[:digit:]]/g' -e 's/\./\\./g')
 # regex_str="^[[:space:]]+${version_regex_str}(?!-rc|-dev|-pre).*"
 regex_str="^[[:space:]]+${version_regex_str}(-p[[:digit:]]+)?$"
 rbenv install -l | grep -P "${regex_str}" | sort --version-sort | tail -n 1 | sed -e 's/[[:blank:]]//g'
