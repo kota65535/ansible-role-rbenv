22e0deb6d1e4a0081c5a20a1af205b48e2cf8f0f
kota65535 <kota65535@gmail.com>
2017-01-05 22:37:39 +0900

    adopt to my new blockinfile module

--- tasks/get_latest_version.sh
+++ tasks/get_latest_version.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
-tmpdir=$(mktemp -d)
+tmpdir=$(mktemp -d /tmp/XXXX)
 git clone -q $1 ${tmpdir}
 git clone -q $2 ${tmpdir}/plugins/ruby-build
 export RBENV_ROOT=${tmpdir}
 export PATH="$RBENV_ROOT/bin:$PATH"
 eval "$(${RBENV_ROOT}/bin/rbenv init -)"
 
-version_regex_str=$(echo "$3" | sed -e 's/x/[[:digit:]]/g' -e 's/\./\\./g')
+version_regex_str=$(echo "$3" | perl -pe 's/(?<!rb)x/[[:digit:]]+/g; s/\./\\./g')
 # regex_str="^[[:space:]]+${version_regex_str}(?!-rc|-dev|-pre).*"
 regex_str="^[[:space:]]+${version_regex_str}(-p[[:digit:]]+)?$"
-rbenv install -l | grep -P "${regex_str}" | sort --version-sort | tail -n 1 | sed -e 's/[[:blank:]]//g'
+rbenv install -l | perl -nlE "say if /${regex_str}/" | php -r '$a=array_map("trim",file("php://stdin"));usort($a,"version_compare");echo(join("\n",$a))."\n";' | tail -n 1 | sed -e 's/[[:blank:]]//g'
+
