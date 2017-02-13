bdba018a8e542b52dd58a312d6d787a518ffee72
kota65535 <kota65535@gmail.com>
2017-02-13 09:11:36 +0900

    modify get_latest_verison

--- tasks/get_latest_version.sh
+++ tasks/get_latest_version.sh
@@ -8,7 +8,7 @@ export PATH="$RBENV_ROOT/bin:$PATH"
 eval "$(${RBENV_ROOT}/bin/rbenv init -)"
 
 # Make regex pattern by replacing 'x' with \d
-version_regex_str=$(echo "$3" | perl -pe 's/X/\\d/g; s/\./\\./g')
+version_regex_str=$(echo "$3" | perl -pe 's/X/\\d+/g; s/\./\\./g')
 # Consider patch number (exclude rc, dev, pre)
 regex_str="^\s*${version_regex_str}(-p\d+)?$"
 rbenv install -l | perl -nle "print if /${regex_str}/" | perl -e 'use Sort::Versions; print sort versions <STDIN>' | tail -n 1 | perl -pe 's/\s//g'
